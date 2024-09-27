c5d6edb6afc7f42e0d101bd765190964a5706911
Edgar Garcia <aedavila@uninorte.edu.co>
2024-09-26 21:07:21 -0500

    aaaaa

--- data/dataPromoImages.js
+++ data/dataPromoImages.js
@@ -1,20 +1,23 @@
+import iniciotop from "../assets/iniciotop.svg";
+import iniciomid from "../assets/iniciomid.svg";
+import iniciolow from "../assets/iniciolow.svg";
+
 const promoImages = [
-    {
-      id: 1,
-      src: "/assets/iniciotop.svg",
-      alt: "Imagen promocional 1"
-    },
-    {
-      id: 2,
-      src: "/assets/iniciomid.svg",
-      alt: "Imagen promocional 2"
-    },
-    {
-      id: 3,
-      src: "/assets/iniciolow.svg",
-      alt: "Imagen promocional 3"
-    }
-  ];
-  
-  module.exports = promoImages;
-  
+  {
+    id: 1,
+    src: iniciotop,  // Imported image
+    alt: "Imagen promocional 1",
+  },
+  {
+    id: 2,
+    src: iniciomid,  // Imported image
+    alt: "Imagen promocional 2",
+  },
+  {
+    id: 3,
+    src: iniciolow,  // Imported image
+    alt: "Imagen promocional 3",
+  },
+];
+
+export default promoImages;
