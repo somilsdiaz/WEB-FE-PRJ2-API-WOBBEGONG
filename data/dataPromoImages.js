ad839f3443a1a7a562b7cd63279596a49ab0039b
Edgar Garcia <aedavila@uninorte.edu.co>
2024-09-26 21:40:08 -0500

    Johaaaaaaaaaaaaaaaaan4

--- data/dataPromoImages.js
+++ data/dataPromoImages.js
@@ -1,17 +1,17 @@
 const promoImages = [
   {
     id: 1,
-    src: "https://web-fe-html-css-prj2-wobbegong.onrender.com/src/assets/iniciotop.svg",
+    src: "https://web-fe-html-css-prj2-wobbegong.onrender.com/assets/iniciotop.svg",
     alt: "Imagen promocional 1",
   },
   {
     id: 2,
-    src: "https://web-fe-html-css-prj2-wobbegong.onrender.com/src/assets/iniciomid.svg",
+    src: "https://web-fe-html-css-prj2-wobbegong.onrender.com/assets/iniciomid.svg",
     alt: "Imagen promocional 2",
   },
   {
     id: 3,
-    src: "https://web-fe-html-css-prj2-wobbegong.onrender.com/src/assets/iniciolow.svg",
+    src: "https://web-fe-html-css-prj2-wobbegong.onrender.com/assets/iniciolow.svg",
     alt: "Imagen promocional 3",
   },
 ];
