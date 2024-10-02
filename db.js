579150ace5293f17705ccde4edf3f86064cd84e2
Johan Vergara <avjohan@uninorte.edu.co>
2024-10-01 21:52:56 -0500

    add module export to db

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const dataCategory = require("./data/dataCategory");
 const promoImages = require("./data/dataPromoImages");
+const datafhp = require("./data/data_fhp");
 
 
 module.exports = () => {
   return {
-   dataCategory,promoImages,
+   dataCategory,promoImages,datafhp
   };
 };
